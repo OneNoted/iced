52e636696d4d880e56d115771370cad6309fb3bd
MalpenZibo <zibo.camito@gmail.com>
2025-12-01 20:54:46 +0100

    fix lag

--- winit/src/program.rs
+++ winit/src/program.rs
@@ -1249,13 +1249,19 @@ async fn run_instance<'a, P, C>(
                             continue;
                         };
 
+                        let mut scaled_window_size =
+                            window.state.logical_size()
+                                * window.state.app_scale_factor() as f32;
+                        scaled_window_size.width =
+                            scaled_window_size.width.trunc();
+                        scaled_window_size.height =
+                            scaled_window_size.height.trunc();
+
                         // XX must force update to corner radius before the surface is committed.
                         #[cfg(feature = "wayland")]
                         if window.viewport_version
                             != window.state.viewport_version()
-                            || window.size()
-                                != window.state.logical_size()
-                                    * window.state.app_scale_factor() as f32
+                            || window.size() != scaled_window_size
                         {
                             platform_specific_handler.send_wayland(
                                 platform_specific::Action::ResizeWindow(id),
